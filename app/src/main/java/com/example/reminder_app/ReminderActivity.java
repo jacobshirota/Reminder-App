b65584a4ef3a6bb3bfe5611bacb3a5023bd9e2b0
jacobshirota <114620967+jacobshirota@users.noreply.github.com>
2022-11-16 20:56:02 -0800

    Completed initial stages of activity_reminder: imageButtons to navigate back to MainActivity, AddActivity, and to delete reminder(to be implemented later) textViews to display reminder title, date, time, and description. Can also implement other features if implemented in AddActivity concurrently (eg. priority, groups, location, etc.)

--- app/src/main/java/com/example/reminder_app/ReminderActivity.java
+++ app/src/main/java/com/example/reminder_app/ReminderActivity.java
@@ -1,6 +1,10 @@
 package com.example.reminder_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +13,17 @@ public class ReminderActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_reminder);
+    }
+
+    public void goToAddActivity(View view) {
+        Intent intent = new Intent(this, AddActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
